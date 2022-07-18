6d526366f87a5c7a4ebf1f30864e18ba3f6f387f
Jesus Romero <jesusr1297@outlook.com>
2022-07-17 20:10:33 -0500

    changed settings for templates location, added CRUD to orders

--- crm/views.py
+++ crm/views.py
@@ -1,15 +1,40 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import *
 from django.shortcuts import render
+from django.urls import reverse
 
 
 class OrderView(ListView):
     model = Order
     template_name = 'crm/order.html'
+    ordering = ['-id']
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    fields = ['lider', 'customer', 'quantity_ordered']
+
+    def get_success_url(self):
+        return reverse('home')
 
 
 class OrderDetailView(DetailView):
     model = Order
+
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('home')
+
+
+class OrderDeleteView(DeleteView):
+    model = Order
+
+    def get_success_url(self):
+        return reverse('home')
 
 
 class CustomersView(ListView):
