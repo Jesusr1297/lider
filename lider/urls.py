263ed06d31e5a9f092056e1a55273fa3ff16cd8f
Jesus Romero <jesusr1297@outlook.com>
2022-07-30 22:59:51 -0500

    Added lider-detail and lider form to lider

--- lider/urls.py
+++ lider/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import (LiderView, LiderCreateView, LiderDetailView)
 
 urlpatterns = [
     path('', LiderView.as_view(), name='lider'),
+    path('crear/', LiderCreateView.as_view(), name='lider-create'),
+    path('detalle/<int:pk>/', LiderDetailView.as_view(), name='lider-detail'),
+
 ]
--- lider/views.py
+++ lider/views.py
@@ -1,5 +1,7 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
 
 from orders.models import Lider
 
@@ -7,3 +9,18 @@ from orders.models import Lider
 class LiderView(ListView):
     model = Lider
     template_name = 'lider/lider.html'
+
+
+class LiderCreateView(SuccessMessageMixin, CreateView):
+    model = Lider
+    template_name = 'lider/lider_form.html'
+    success_message = 'Se ha creado un nuevo LIDER'
+    success_url = reverse_lazy('lider')
+    fields = '__all__'
+
+
+class LiderDetailView(DetailView):
+    model = Lider
+    template_name = 'lider/lider_detail.html'
+
+
